6723db26324512f9af5cce6bd8a38773fde4ad25
Phat <vanphat@siliconstraits.com>
2016-08-24 00:48:26 +0700

    step 5: update filesToCache array

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,23 @@
 var cacheName = 'weatherPWA-step-5-1'; // for versioning
-var filesToCache = [];
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/clear.png',
+  '/images/cloudy-scattered-showers.png',
+  '/images/cloudy.png',
+  '/images/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/images/partly-cloudy.png',
+  '/images/rain.png',
+  '/images/scattered-showers.png',
+  '/images/sleet.png',
+  '/images/snow.png',
+  '/images/thunderstorm.png',
+  '/images/wind.png'
+];
 
 /*
  When the service worker is registered,
@@ -29,4 +47,4 @@ self.addEventListener('activate', function (event) {
       }))
     })
   )
-})
+});
