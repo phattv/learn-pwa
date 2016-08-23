61ec7d3d3d5d62bd23982d505cb041906e7e2994
Phat <vanphat@siliconstraits.com>
2016-08-24 00:07:51 +0700

    step 4: prepare static local weather forcast, get rid of fakeForecast

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,32 @@
   'use strict';
 
   // Insert injected weather forecast here
-
+  var initialWeatherForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
 
   var app = {
     isLoading: true,
@@ -180,7 +205,7 @@
     }
   };
   // Uncomment the line below to test the app with fake data
-  app.updateForecastCard(fakeForecast);
+  // app.updateForecastCard(fakeForecast);
 
   // Add code to save the users list of subscribed cities here
 
